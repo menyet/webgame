016d068b3d5954e4fbeb9963bfcd1481d849b356
menyet <info@mandli.sk>
2015-10-11 22:15:16 +0200

    basic controllers

---
+++ src/app/Http/Controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MainController extends Controller
+{
+    public function home() {
+        return view('welcome');
+    }
+    
+    
+}
+
+?>
--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -1,7 +1,62 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        <h1>Laravel Quickstart</h1>
+    <head>
+        <title>Laravel</title>
 
-        @yield('content')
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                clear:both;
+                background-color: #6495ed;
+                height:500px;
+                width:1000px; 
+                position: relative;
+            }
+
+            .content {
+                
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                height: 30%;
+                width: 50%;
+                margin: -15% 0 0 -25%;
+            }
+            
+            
+            .title {
+                font-size: 96px;
+            }
+            
+            .menu {
+                afloat: right;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="menu">
+            <a href="{{ action('MainController@home') }}">Home</a>
+            <a href="{{ action('MainController@home') }}">Home</a>
+            <a href="{{ action('MainController@home') }}">Home</a>
+        </div>
+        
+        <div class="container">
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
     </body>
 </html>
