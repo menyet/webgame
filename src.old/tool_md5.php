3e47aa636e4a61715340c0fc06dd7fe58db3b394
menyet <info@mandli.sk>
2015-10-12 22:33:53 +0200

    Add background

--- src.old/tool_md5.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<form action="'.$_SERVER['PHP_SELF'].'" method="get">';
-echo 'zadaj slovo: <input type="text" name="slovo"> ';
-echo '<input type="submit"><hr>';
-if (isset($_GET['slovo']) && $_GET['slovo'] != ""){
-	echo 'md5 hash: '.md5($_GET['slovo']);
-}
-
-?>
